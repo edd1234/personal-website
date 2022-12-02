14e8994438af408475d80a21172df91c8ac1a100
edd1234 <63986599+edd1234@users.noreply.github.com>
2022-12-02 13:33:28 -0600

    Add files via upload

---
+++ webpage.js
@@ -0,0 +1,14 @@
+function goCityProject() {
+    location.href= "city-project-page.html"
+}
+
+function goAboutMe() {
+    location.href= "about-me.html"
+}
+
+function goMainPage() {
+    location.href= "main.html"
+}
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
